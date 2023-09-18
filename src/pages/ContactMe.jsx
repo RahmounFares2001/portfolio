9d38afc4b2ce902de708310c390e11b240ac5318
RahmounFares2001 <140554206+RahmounFares2001@users.noreply.github.com>
2023-09-18 18:44:32 +0200

    Update ContactMe.jsx

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -15,7 +15,7 @@ export default function ContactMe() {
                 <textarea type="message" rows="5" placeholder="Your Message"
                 className="lg:w-2/5 sm:w-3/5 w-4/5 bg-gray-100 p-3 text-gray-800 border-2 border-gray-600" />
 
-                <button className="lg:w-2/5 lg:h-14 sm:w-3/5 w-4/5 border-2 
+                <button className="lg:w-2/5 lg:h-14 h-14 sm:w-3/5 w-4/5 border-2 
                 border-gray-600 bg-gradient-to-r from-gray-400 to-transparent hover:bg-gradient-to-l
                  hover:from-red-950 hover:to-transparent text-gray-950 hover:text-gray-400
                     hover:transition-colors hover:duration-1000 cursor-pointer active:animate-fade-in">Sumbit</button>
