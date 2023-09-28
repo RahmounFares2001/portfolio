85784951256b816003f149afbe66398cf2c13c13
RahmounFares2001 <faresrahm40@gmail.com>
2023-09-28 13:01:33 +0200

    com

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
     return(
         <div className="flex justify-center mt-32">
             <div className="border-t-2 border-white w-max text-center sm:text-base text-xs">
-                © Rahmoun Fares Abdelhamid 2023. All rights reserved.
+                © Rahmoun Fares 2023. All rights reserved.
             </div>
         </div>
     )
