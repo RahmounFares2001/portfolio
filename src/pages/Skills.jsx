af164e108a03678052eb040c679f63f3e992c462
RahmounFares2001 <faresrahm40@gmail.com>
2023-12-22 12:47:45 +0100

    test

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -21,8 +21,8 @@ export default function Skills(){
 
     return(
         <div className="mt-16 flex flex-col  items-center  animate-slideIn">
-            <div className="lg:w-7/12 sm:w-8/12 w-9/12 flex justify-center">
-                <button className={`lg:w-4/6 lg:h-14 h-14 w-4/5 border-2 md:text-xl text-base
+            <div className="lg:w-7/12 sm:w-8/12 w-11/12 flex justify-center">
+                <button className={`lg:w-4/6 lg:h-14 sm:h-14 h-12 w-4/5 border-2 md:text-xl text-base
                     border-gray-600 bg-gradient-to-r from-gray-400 to-transparent hover:bg-gradient-to-l
                     hover:from-red-950 hover:to-transparent text-gray-950 hover:text-gray-400
                         hover:transition-colors hover:duration-1000 cursor-pointer ${coloringBtn('tech')}`}
@@ -30,7 +30,7 @@ export default function Skills(){
                             btnId("tech");
                             setActive("active1");
                         }} >Technical skills</button>
-                <button className={`lg:w-4/6 lg:h-14 h-14 w-4/5 border-2 md:text-xl text-base
+                <button className={`lg:w-4/6 lg:h-14 sm:h-14 h-12 w-4/5 border-2 md:text-xl text-base
                     border-gray-600 bg-gradient-to-r from-gray-400 to-transparent hover:bg-gradient-to-l
                     hover:from-red-950 hover:to-transparent text-gray-950 hover:text-gray-400
                         hover:transition-colors hover:duration-1000 cursor-pointer ${coloringBtn('soft')}`}
