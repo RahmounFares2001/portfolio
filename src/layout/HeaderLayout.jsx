43ad8177e4065b946f34b9cb7317d8ce7770b350
RahmounFares2001 <faresrahm40@gmail.com>
2023-12-24 21:43:10 +0100

    com

--- src/layout/HeaderLayout.jsx
+++ src/layout/HeaderLayout.jsx
@@ -14,6 +14,7 @@ import CloseIcon from '@mui/icons-material/Close';
 
 // styles
 import styles from "./styles/styles.css"
+import { color } from "framer-motion";
 
 export default function HeaderLayout() {
 
@@ -34,6 +35,11 @@ export default function HeaderLayout() {
     const activeStyle = "text-red-500 font-bold border-b border-red-600 transition-colors duration-1000"
     const coloring = (id) => {
         return active === id ? activeStyle : "";
+    };
+
+    const activeStyleResponsive = "text-red-500 font-bold transition-colors duration-1000";
+    const coloringResponsive = (id) => {
+        return active === id ? activeStyleResponsive : "";
     };
 
     return(
@@ -92,29 +98,42 @@ export default function HeaderLayout() {
                     md:hidden flex flex-col gap-8 text-4xl animate-fade-in
                     "> 
 
-                        <NavLink to="/" className=" hover:text-red-500 hover:font-bold hover:border-b
+                        <NavLink to="/" className={` hover:text-red-500 hover:font-bold hover:border-b
                          hover:border-red-600  text-center hover:transition-colors hover:duration-1000 
-                          lg:w-16 md:w-12  animate-responsive-menu1" onClick={showMenu}>Home</NavLink>
+                          lg:w-16 md:w-12  animate-responsive-menu1 ${coloringResponsive("home")}`} onClick={()=>{
+                            showMenu();
+                            btnId("home")
+                          }}>Home</NavLink>
 
-                        <NavLink to="/projects" className="hover:text-red-500 hover:font-bold hover:border-b
+                        <NavLink to="/projects" className={`hover:text-red-500 hover:font-bold hover:border-b
                          hover:border-red-600 text-center hover:transition-colors hover:duration-1000
-                         lg:w-24 md:w-24 opacity-0 animate-responsive-menu2" onClick={showMenu}>Projects</NavLink>
+                         lg:w-24 md:w-24 opacity-0 animate-responsive-menu2 ${coloringResponsive("projects")}`} onClick={()=>{
+                            showMenu()
+                            btnId("projects")}}>Projects</NavLink>
 
-                        <NavLink to="/resume" className="hover:text-red-500 hover:font-bold hover:border-b
+                        <NavLink to="/resume" className={`hover:text-red-500 hover:font-bold hover:border-b
                          hover:border-red-600  text-center hover:transition-colors hover:duration-1000
-                         lg:w-20 md:w-16 opacity-0 animate-responsive-menu3" onClick={showMenu}>Resume</NavLink>
+                         lg:w-20 md:w-16 opacity-0 animate-responsive-menu3 ${coloringResponsive("resume")}`} onClick={()=> {
+                            showMenu()
+                            btnId("resume")}}>Resume</NavLink>
 
-                        <NavLink to="/about" className="hover:text-red-500 hover:font-bold hover:border-b
+                        <NavLink to="/about" className={`hover:text-red-500 hover:font-bold hover:border-b
                          hover:border-red-600  text-center hover:transition-colors hover:duration-1000
-                          lg:w-20 md:w-16 opacity-0 animate-responsive-menu4" onClick={showMenu}>About</NavLink>
+                          lg:w-20 md:w-16 opacity-0 animate-responsive-menu4 ${coloringResponsive("about")}`} onClick={()=> {
+                            showMenu()
+                            btnId("about")}}>About</NavLink>
 
-                        <NavLink to="/skills" className="hover:text-red-500 hover:font-bold hover:border-b
+                        <NavLink to="/skills" className={`hover:text-red-500 hover:font-bold hover:border-b
                          hover:border-red-600  text-center hover:transition-colors hover:duration-1000
-                         lg:w-20 md:w-16 opacity-0 animate-responsive-menu5" onClick={showMenu}>skills</NavLink>
+                         lg:w-20 md:w-16 opacity-0 animate-responsive-menu5 ${coloringResponsive("skills")}`} onClick={()=> {
+                            showMenu()
+                            btnId("skills")}}>skills</NavLink>
 
-                         <NavLink to="/contactMe" className="hover:text-red-500 hover:font-bold hover:border-b
+                         <NavLink to="/contactMe" className={`hover:text-red-500 hover:font-bold hover:border-b
                          hover:border-red-600  text-center hover:transition-colors hover:duration-1000
-                         lg:w-20 md:w-16 opacity-0 animate-responsive-menu6" onClick={showMenu}>Contact</NavLink>
+                         lg:w-20 md:w-16 opacity-0 animate-responsive-menu6 ${coloringResponsive("contactMe")}`} onClick={()=>{
+                            showMenu()
+                            btnId("contactMe")}}>Contact</NavLink>
 
                     </div>:
             <main>
